f7c95734213a05f26432216eb1231af951b3644d
phtre57 <phtre57@gmail.com>
2018-02-09 08:45:58 -0800

    thread fonctionne seulement avec gdb...

--- threads.c
+++ threads.c
@@ -21,18 +21,16 @@ typedef struct {
 /* Voici la fonction correspondant a chaque thread - à compléter */
 void  *ProduitScalaire(void *data) {
     ParametresThread *pParam = (ParametresThread*)data;
-    double res = 0;
+    printf("pRes_ptr_start %p\n", pParam->pResultat);
+    *pParam->pResultat = 0;
     int i;
     for (i = 0; i < M; i++){
-    	printf("%f\n", pParam->Vecteur1[i]);
-    	printf("%f\n", pParam->Vecteur2[i]);
-    	res += pParam->Vecteur1[i] * pParam->Vecteur2[i];
-    	printf("Inter res: %f\n", res);
+    	*pParam->pResultat += pParam->Vecteur1[i] * pParam->Vecteur2[i];
     }
 
-    printf("Total res: %f\n", res);
+    printf("Total res: %f\n", *pParam->pResultat);
 
-    pParam->pResultat = &res;
+    printf("pParam_ptr_end %p\n", pParam->pResultat);
     printf("pRes: %f\n", *pParam->pResultat);
 
     pthread_exit(NULL);
@@ -78,25 +76,25 @@ int main(int argc, char *argv[]) {
     //AfficherMatrice(&H[0][0],M,P);
     // Votre code de creation de thread commence ici...
 
-    double index_test = 0;
-    double *ptr_test = &index_test;
+    double index_test[10] = {1,1,1,1,1};
+    double *ptr_test = &index_test[2];
     double v1_test[10] = {1.0,2.0,3.0,4.0,5.0,6.0,7.0,8.0,9.0,10.0};
     double v2_test[10] = {1.0,2.0,3.0,4.0,5.0,6.0,7.0,8.0,9.0,10.0};
-    ParametresThread test = {0, M, *v1_test, *v2_test, *ptr_test};
+    ParametresThread test;
 
+    test.pResultat = ptr_test;
+    test.Longueur = M;
+    test.iThread = threads[0];
     for (int i = 0; i < M; i++){
     	test.Vecteur1[i] = v1_test[i];
     	test.Vecteur2[i] = v2_test[i];
     }
-
     
-    for (int z = 0; z < M; z++){
-    	printf("vect1 %f\n", test.Vecteur1[z]);
+    int status = pthread_create(&threads[0], NULL, ProduitScalaire, (void *) &test);
+
+    for (int i = 0; i < 5; i++){
+    	printf("index_test res %f\n", index_test[i]);
     }
-    pthread_create(&threads[0], NULL, ProduitScalaire, (void *) &test);
-    printf("ptr_test %f\n", *ptr_test);
-
-
 
     // Et se termine essentiellement ici...
     printf("Main(): Tous les threads ont termines! On affiche la reponse...\n");
